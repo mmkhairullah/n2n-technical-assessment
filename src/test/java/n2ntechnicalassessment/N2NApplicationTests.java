a3f5075bf201ce1b10d82c2048038ab8d510cca8
mmkhairullah <mkhrllh.work@hotmail.com>
2024-10-26 03:18:01 +0800

    feature: rename

--- src/test/java/n2ntechnicalassessment/N2NApplicationTests.java
+++ src/test/java/n2ntechnicalassessment/N2NApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BookstoreApplicationTests {
+class N2NApplicationTests {
 
 	@Test
 	void contextLoads() {
