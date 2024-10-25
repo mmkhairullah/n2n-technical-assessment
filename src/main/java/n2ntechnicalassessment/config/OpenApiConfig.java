f5997ae1a3002e4414c5110cd8370cb2b70b1f62
mmkhairullah <mkhrllh.work@hotmail.com>
2024-10-26 02:17:35 +0800

    feature: edit config file

--- src/main/java/n2ntechnicalassessment/config/OpenApiConfig.java
+++ src/main/java/n2ntechnicalassessment/config/OpenApiConfig.java
@@ -13,8 +13,8 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Backend Bookstore Customers API")
-                        .description("For Etiqa Technical Assessment - 16 January 2024 - Java Spring Boot")
+                        .title("GAMECARD API")
+                        .description("For N2N Technical Assessment - 25 OCT 2024 - Java Spring Boot")
                         .version("v1")
                         .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
                 );
